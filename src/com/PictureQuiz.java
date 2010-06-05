0944ab3a65de59025b424f6a077a9bb138f15ade
U-CORPORATE\balajid <balajid@Balajid.corporate.thoughtworks.com>
2010-06-05 18:21:45 +0530

    parameterized size

--- src/com/PictureQuiz.java
+++ src/com/PictureQuiz.java
@@ -1,12 +1,11 @@
 package com;
 
-import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
@@ -14,6 +13,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -22,18 +22,21 @@ import javax.swing.UIManager;
 
 public class PictureQuiz extends JApplet {
 
-	private static int DIMENSION = 3;
+	private int dimension;
 	private static final long serialVersionUID = 1L;
 	private List<String> questions;
 
 	@Override
 	public void init() {
+		dimension = Integer.parseInt(getParameter("size"));
 		setSize(600, 600);
+		String[] qs = getParameter("questions").split(",");
 		questions = new ArrayList<String>();
-		for (int i = 0; i < 9; i++)
-			questions.add("This is question number " + i);
+		for (int i = 0; i < dimension * dimension; i++) {
+			questions.add(qs[i]);
+		}
 		try {
-			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception ex) {
 			Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
 		}
@@ -42,16 +45,26 @@ public class PictureQuiz extends JApplet {
 	@Override
 	public void start() {
 		try {
-			prepareFrame(getContentPane());
+			prepareFrame(getParameter("image"));
 		} catch (Exception e) {
 			Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, e);
 		}
 	}
 
-	private void prepareFrame(final Container container) throws MalformedURLException, IOException {
-		JPanel panel = new BackgroundPanel(new GridLayout(DIMENSION, DIMENSION));
+	private void prepareFrame(String imageName) throws MalformedURLException, IOException {
+		final Image image = ImageIO.read(new File(imageName));
+		JPanel panel = new JPanel(new GridLayout(dimension, dimension)) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+			}
+		};
+
 		JButton button;
-		for (int i = 0; i < DIMENSION * DIMENSION; i++) {
+		for (int i = 0; i < dimension * dimension; i++) {
 			button = new JButton(String.valueOf(i + 1));
 			button.setFocusable(false);
 			button.addActionListener(new ActionListener() {
@@ -59,31 +72,13 @@ public class PictureQuiz extends JApplet {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					JButton me = (JButton) e.getSource();
-					JOptionPane.showMessageDialog(container, questions.get(Integer.parseInt(me.getText())), "Quiz",
+					JOptionPane.showMessageDialog(getContentPane(), questions.get(Integer.parseInt(me.getText()) - 1), "Quiz",
 							JOptionPane.QUESTION_MESSAGE);
 					me.setVisible(false);
 				}
 			});
 			panel.add(button);
 		}
-		container.add(panel);
-	}
-}
-
-class BackgroundPanel extends JPanel {
-	private static final long serialVersionUID = 1L;
-	Image image;
-
-	public BackgroundPanel(LayoutManager layoutManager) throws MalformedURLException, IOException {
-		super(layoutManager);
-		image = javax.imageio.ImageIO.read(new java.net.URL("file:///home/balaji/Downloads/im/06qa-done.jpg"));
-	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (image != null) {
-			g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
-		}
+		getContentPane().add(panel);
 	}
 }
