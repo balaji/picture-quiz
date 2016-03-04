f6e56ee62e7e7195175827788e4e1d4561e1a0c9
Balaji Damodaran <damodaran.balaji@gmail.com>
2016-03-05 00:00:54 +0530

    A working flexible grid. checkpoint

--- js/build/main.js
+++ js/build/main.js
@@ -1,5 +1,133 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, world!'
-), document.getElementById('container'));
+var Row = React.createClass({
+  displayName: 'Row',
+
+  hide: function () {
+    console.log(this.props);
+  },
+
+  render: function () {
+    var columns = [];
+    for (var i = 1; i <= this.props.columns; i++) {
+      columns.push(React.createElement(
+        'td',
+        { onClick: this.hide, key: i + this.props.multiplier },
+        i + this.props.multiplier
+      ));
+    }
+    return React.createElement(
+      'tr',
+      null,
+      columns
+    );
+  }
+});
+
+var Table = React.createClass({
+  displayName: 'Table',
+
+  render: function () {
+    var tbody = [];
+    for (var i = 0; i < this.props.rows - 1; i++) {
+      tbody.push(React.createElement(Row, { multiplier: this.props.columns * i, key: i, columns: this.props.columns }));
+    }
+
+    if (this.props.lastRow === 0) {
+      var multiplier = this.props.startsWith ? this.props.startsWith : this.props.columns * (this.props.rows - 1);
+      tbody.push(React.createElement(Row, { multiplier: multiplier, key: multiplier - 1, columns: this.props.columns }));
+      return React.createElement(
+        'table',
+        null,
+        React.createElement(
+          'tbody',
+          null,
+          tbody
+        )
+      );
+    }
+
+    var topHeight = Math.round(400 / this.props.rows * (this.props.rows - 1));
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'div',
+        { style: { height: topHeight + 'px' } },
+        React.createElement(
+          'table',
+          null,
+          React.createElement(
+            'tbody',
+            null,
+            tbody
+          )
+        )
+      ),
+      React.createElement(
+        'div',
+        { style: { height: 400 - topHeight + 'px' } },
+        React.createElement(Table, { startsWith: (this.props.rows - 1) * this.props.columns, rows: 1, columns: this.props.lastRow, lastRow: 0 })
+      )
+    );
+  }
+});
+
+var Frame = React.createClass({
+  displayName: 'Frame',
+
+  render: function () {
+    return React.createElement(
+      'div',
+      { className: 'frame' },
+      React.createElement(Table, { rows: this.props.rows, columns: this.props.columns, lastRow: this.props.lastRow })
+    );
+  }
+});
+
+var Picker = React.createClass({
+  displayName: 'Picker',
+
+  getInitialState: function () {
+    return { count: this.props.initialCount };
+  },
+  decrement: function () {
+    this.setState({ count: this.state.count - 1 });
+  },
+
+  increment: function () {
+    this.setState({ count: this.state.count + 1 });
+  },
+
+  render: function () {
+    var sqrt = Math.sqrt(this.state.count);
+    var rows = Math.round(sqrt);
+    var columns = Math.ceil(sqrt);
+    var lastRow = this.state.count % columns;
+
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'button',
+        { onClick: this.decrement },
+        '-'
+      ),
+      this.state.count,
+      React.createElement(
+        'button',
+        { onClick: this.increment },
+        '+'
+      ),
+      React.createElement(Frame, { rows: rows, columns: columns, lastRow: lastRow })
+    );
+  }
+});
+
+var Container = React.createClass({
+  displayName: 'Container',
+
+  render: function () {
+    return React.createElement(Picker, { initialCount: 100 });
+  }
+});
+
+ReactDOM.render(React.createElement(Container, null), document.getElementById('container'));
--- js/src/main.js
+++ js/src/main.js
@@ -1,4 +1,92 @@
-ReactDOM.render(
-      <h1>Hello, world!</h1>,
-        document.getElementById('container')
+var Row = React.createClass({
+  hide: function() {
+    console.log(this.props);
+  },
+
+  render: function() {
+    var columns = [];
+    for(var i = 1; i <= this.props.columns; i++) {
+      columns.push(<td onClick={this.hide} key={i + this.props.multiplier}>
+        {i + this.props.multiplier}</td>);
+    }
+    return(<tr>{columns}</tr>);
+  }
+});
+
+var Table = React.createClass({
+  render: function() {
+    var tbody = [];
+    for(var i = 0; i < this.props.rows - 1; i++) {
+      tbody.push(<Row multiplier={this.props.columns * i} key={i} columns={this.props.columns} />);
+    }
+
+    if(this.props.lastRow === 0) {
+      var multiplier = (this.props.startsWith) ? this.props.startsWith : this.props.columns * (this.props.rows - 1);
+      tbody.push(<Row multiplier={multiplier} key={multiplier - 1} columns={this.props.columns} />);
+      return (
+        <table><tbody>{tbody}</tbody></table>
+      );
+    }
+
+    var topHeight = Math.round((400 / this.props.rows) * (this.props.rows - 1));
+    return (
+      <div>
+        <div style={{height: topHeight + 'px'}}>
+          <table><tbody>{tbody}</tbody></table>
+        </div>
+        <div style={{height: (400 - topHeight) + 'px'}}>
+          <Table startsWith={(this.props.rows - 1) * this.props.columns} rows={1} columns={this.props.lastRow} lastRow={0} />
+        </div>
+      </div>
     );
+  }
+});
+
+var Frame = React.createClass({
+  render: function () {
+    return (
+      <div className="frame">
+        <Table rows={this.props.rows} columns={this.props.columns} lastRow={this.props.lastRow} />
+      </div>
+    );
+  }
+});
+
+var Picker = React.createClass({
+  getInitialState: function() {
+    return {count: this.props.initialCount};
+  },
+  decrement: function() {
+    this.setState({count: this.state.count - 1});
+  },
+
+  increment: function() {
+    this.setState({count: this.state.count + 1});
+  },
+
+  render: function() {
+    var sqrt = Math.sqrt(this.state.count);
+    var rows = Math.round(sqrt);
+    var columns = Math.ceil(sqrt);
+    var lastRow = this.state.count % columns;
+
+    return (
+      <div>
+        <button onClick={this.decrement}>-</button>
+        {this.state.count}
+        <button onClick={this.increment}>+</button>
+        <Frame rows={rows} columns={columns} lastRow={lastRow}/>
+      </div>
+    );
+  }
+});
+
+var Container = React.createClass({
+  render: function() {
+    return (
+      <Picker initialCount={100}/>
+    );
+  }
+});
+
+ReactDOM.render(<Container />, document.getElementById('container'));
